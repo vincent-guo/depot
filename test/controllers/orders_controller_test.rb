a1e49318396c949e0fd9cebe4eedaa141b1dc170
Vincent Guo <vincent.weiguo@gmail.com>
2020-05-28 11:17:52 +0800

    logging in

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -17,7 +17,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    post line_items_url, param: { product_id: products(:ruby).id }
+    post line_items_url, params: { product_id: products(:ruby).id }
 
     get new_order_url
     assert_response :success
